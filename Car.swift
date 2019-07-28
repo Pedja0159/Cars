12d9940f79b29ea19cdf01e4b6bf4bfe5c12734e
Pedja <51844353+Pedja0159@users.noreply.github.com>
2019-07-28 17:23:45 +0200

    Add files via upload

---
+++ Car.swift
@@ -0,0 +1,39 @@
+//
+//  Team.swift
+//  Automobili2
+//
+//  Created by Pejo on 7/9/19.
+//  Copyright © 2019 pejo015. All rights reserved.
+//
+
+import UIKit
+
+
+class Car {
+    
+    var name: String?
+    var price: Double?
+    var age: Int?
+    var model: String?
+    var image: UIImage?
+    
+    
+    init(name:String, price:Double, age:Int, model:String, imageName:String) {
+            self.name = name
+            self.price = price
+            self.age = age
+            self.model = model
+            self.image = UIImage(named: imageName)
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
+
+
+
